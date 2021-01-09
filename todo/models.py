d20e237ede63f0f3e121aea45eaf5475bc943a98
Isaac Chung <ichung08@students.cs.ubc.ca>
2021-01-09 14:09:19 -0800

    setting up

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,6 @@
+from datetime import datetime
 from django.db import models
 
+
 # Create your models here.
+
--- todo/urls.py
+++ todo/urls.py
@@ -7,9 +7,6 @@ app_name = 'match'  # setting application namespace
 # Using generic views instead of comment block above
 # DetailView expects primary key value to be called pk
 urlpatterns = [
-    # Example useage
-    # path('', views.IndexView.as_view(), name='index'),
-    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
+
+
 ]
