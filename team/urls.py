c8106a380f2ad37241db71ec7f80f04cc734243f
MyMemIsLeaking <ccffoonngg@gmail.com>
2021-01-09 14:21:01 -0800

    Created stylesheets and htmls

--- team/urls.py
+++ team/urls.py
@@ -7,6 +7,7 @@ app_name = 'team'  # setting application namespace
 # Using generic views instead of comment block above
 # DetailView expects primary key value to be called pk
 urlpatterns = [
+    path('teamhub/', views.teamHub, name='team-hub')
     # Example useage
     # path('', views.IndexView.as_view(), name='index'),
     # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
--- team/views.py
+++ team/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def teamHub(request) {
+    return render(request, 'team-hub/team-hub.html')
+}
