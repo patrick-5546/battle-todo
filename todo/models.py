0d8201d07f908f571219c134408a567712a728b7
Patrick Creighton <pat5546@student.ubc.ca>
2021-01-09 14:08:15 -0800

    setup todo app models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from ..team.models import Player
+
+class Todo(models.Model):
+    user = models.OneToOneField(Player, on_delete=models.CASCADE, primary_key=True)
+    def __str__(self):
+        return 'Primary Key: {}'.format(self.pk)
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=200)
+    completion_status = models.BooleanField(default=False)
+    todo_list = models.ForeignKey(Todo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.task_name
--- todo/urls.py
+++ todo/urls.py
@@ -2,14 +2,11 @@ from django.urls import path
 
 from . import views
 
-app_name = 'match'  # setting application namespace
+app_name = 'todo'  # setting application namespace
 
 # Using generic views instead of comment block above
 # DetailView expects primary key value to be called pk
 urlpatterns = [
-    # Example useage
-    # path('', views.IndexView.as_view(), name='index'),
-    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.login_page, name='login'),
+    path('<int:pk>/todo', views.DetailView.as_view(), name='todo'),
 ]
