c96541c4e3742745509dfeb3d3e90dc91fcd84d6
Isaac Chung <ichung08@students.cs.ubc.ca>
2021-01-09 21:28:59 -0800

    set up todolist form

--- todo/urls.py
+++ todo/urls.py
@@ -7,4 +7,5 @@ app_name = 'todo'  # setting application namespace
 urlpatterns = [
     path('', views.login_page, name='login'),
     path('<int:pk>/todo', views.DetailView.as_view(), name='todo'),
+    # path('search/', views.search, name='search'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,25 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 
-from .models import Todo, Task
+from .models import *
+from .forms import *
+
 
 def login_page(request):
     template = 'todo/login_page.html'
 
     return render(request, template)
 
-def todo_page(request):
-    template = 'todo/todo_page.html'
-
-    return render(request, template)
 
 class DetailView(generic.DetailView):
     model = Todo
     template_name = 'todo/todo_page.html'
+    form = TaskForm()
+    context_object_name = 'todo'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
         # Add in a QuerySet of all the books
+
         context['tasks'] = Task.objects.filter(
             todo_list=Todo.objects.get(pk=self.object.pk))
+        context['form'] = self.form
+
         return context
+
+    def post(self, request, **kwargs):
+        self.object = self.get_object()
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(self.request.path_info)
+
+        return render(request, self.template_name, context=self.get_context_data(**kwargs))
+
+# search works as a "buffer" for when we are obtaining data
+# def search(request):
+#     if request.method == 'GET':
+#         search = request.GET.get('make_task')
+        
+#         print("ok")
+#         return redirect('todo:course', pk=search)
