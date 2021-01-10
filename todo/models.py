bc1ca3746e7a1408a8eb377cc3a8dec0c7669176
alvinkam <alvin.kam.33@gmail.com>
2021-01-09 17:17:19 -0800

    todo field name changed

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from team.models import Player
 
 class Todo(models.Model):
-    user = models.OneToOneField(Player, on_delete=models.CASCADE, primary_key=True)
+    player = models.OneToOneField(Player, on_delete=models.CASCADE, primary_key=True)
     
     def __str__(self):
         return self.user.player_name + " to do list, pk={}".format(self.pk)
