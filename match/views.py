7d5e623d8dce2d33fe80a65450d7c8c12d7de83b
alvinkam <alvin.kam.33@gmail.com>
2021-01-10 05:48:16 -0800

    some styling changes, startgame page revamped, set up psuedo-matchmaking

--- match/views.py
+++ match/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render, redirect
 from .game import run
+from .models import Match
 from team.models import Team, Player
 from django.urls import reverse
 
 # Create your views here.
 
 def startgame(request):
-    return render(request, 'match/startgame.html')
+    match = Match(team1_pk = 1, team2_pk = 2)
+
+    team1 = Team.objects.get(pk = match.team1_pk)
+    team2 = Team.objects.get(pk = match.team2_pk)
+
+    player1 = Player.objects.get(pk = match.team1_pk * 5 - 4)
+    player2 = Player.objects.get(pk = match.team1_pk * 5 - 3)
+    player3 = Player.objects.get(pk = match.team1_pk * 5 - 2)
+    player4 = Player.objects.get(pk = match.team1_pk * 5 - 1)
+    player5 = Player.objects.get(pk = match.team1_pk * 5 - 0)
+    player6 = Player.objects.get(pk = match.team2_pk * 5 - 4)
+    player7 = Player.objects.get(pk = match.team2_pk * 5 - 3)
+    player8 = Player.objects.get(pk = match.team2_pk * 5 - 2)
+    player9 = Player.objects.get(pk = match.team2_pk * 5 - 1)
+    player10 = Player.objects.get(pk = match.team2_pk * 5 - 0)
+
+    team1list = [player1, player2, player3, player4, player5]
+    team2list = [player6, player7, player8, player9, player10]
+    
+    return render(request, 'match/startgame.html', {'team1': team1, 'team2': team2, 'team1list': team1list, 'team2list': team2list})
 
 def results(request):
     team1damage = 0
