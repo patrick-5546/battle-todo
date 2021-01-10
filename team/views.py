2ab2bec8b668de279a6d65f541a56c25711021a8
MyMemIsLeaking <ccffoonngg@gmail.com>
2021-01-10 06:03:34 -0800

    Finished card buttons

--- team/views.py
+++ team/views.py
@@ -1,8 +1,8 @@
 from .models import Team, Player
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 
-# Create your views here.
+from .models import Team, Player
 
 
 def teamHub(request):
@@ -19,4 +19,37 @@ class DetailView(generic.DetailView):
         # Add in a QuerySet of all the books
         context['team_players'] = Player.objects.filter(
             player_team=Team.objects.get(pk=self.object.pk))
+        context['player_leaderboards'] = Player.objects.order_by('')
         return context
+
+def find(request, pk):
+    if request.method == 'POST':
+        player = Player.objects.get(pk=pk)
+        todo_points = player.todo_points
+
+        physical_attack = int(request.POST.get(
+            'physical_attack')) - player.physical_attack
+        physical_defense = int(request.POST.get(
+            'physical_defense')) - player.physical_defense
+        special_attack = int(request.POST.get(
+            'special_attack')) - player.special_attack
+        special_defense = int(request.POST.get(
+            'special_defense')) - player.special_defense
+        speed = int(request.POST.get('speed')) - player.speed
+
+        sum_dif = physical_attack + physical_defense + \
+        special_attack + special_defense + speed
+
+        if sum_dif <= todo_points:
+            player.physical_attack += physical_attack
+            player.physical_defense += physical_defense
+            player.special_attack += special_attack
+            player.special_defense += special_defense
+            player.speed += speed
+            player.todo_points -= sum_dif
+            player.save()
+
+    else:
+        print('failed attempt')
+
+    return redirect('team:team-hub', pk=player.player_team.pk)
