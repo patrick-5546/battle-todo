674c42102327af94e880870a0a093f38322b9f94
Isaac Chung <ichung08@students.cs.ubc.ca>
2021-01-09 16:06:58 -0800

    updating todo model

--- todo/models.py
+++ todo/models.py
@@ -6,7 +6,8 @@ class Todo(models.Model):
     user = models.OneToOneField(Player, on_delete=models.CASCADE, primary_key=True)
     
     def __str__(self):
-        return 'Primary Key: {}'.format(self.pk)
+        return self.user.player_name + " to do list"
+    
 
 class Task(models.Model):
     todo_list = models.ForeignKey(Todo, on_delete=models.CASCADE)
