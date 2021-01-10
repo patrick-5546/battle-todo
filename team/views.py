4ba9525f092169b5793e52ce568e2edc6e25f334
MyMemIsLeaking <ccffoonngg@gmail.com>
2021-01-09 16:23:43 -0800

    debug push

--- team/views.py
+++ team/views.py
@@ -5,9 +5,8 @@ from django.views import generic
 # Create your views here.
 
 
-def teamHub(request) {
+def teamHub(request):
     return render(request, 'team-hub/team-hub.html')
-}
 
 
 class DetailView(generic.DetailView):
